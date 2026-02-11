db4b83a592873dcc026fb45453e30f7762a9750e
Vika_Fernanda <98906257+VikaFernanda@users.noreply.github.com>
2026-02-11 15:22:04 -0300

    Delete ch5 directory

--- ch5/h2/terraform/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "key_name" {
-  description = "The name of the SSH key pair to use for EC2 instances"
-  type        = string
-}
--- ch5/h4/terraform/modules/iam_roles/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "environment" {
-  description = "Environment for the IAM roles"
-  type        = string
-}
-
-variable "owners" {
-  description = "Owner of the IAM roles"
-  type        = string
-}
